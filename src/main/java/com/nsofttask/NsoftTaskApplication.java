45d4415c4c440aa8119c8324a6672335be5597f8
deni997 <deni.dujmovic97@gmail.com>
2022-04-04 15:21:31 +0200

    Add project

---
+++ src/main/java/com/nsofttask/NsoftTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nsofttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsoftTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsoftTaskApplication.class, args);
+	}
+
+}
