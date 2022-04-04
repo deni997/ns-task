117c90dcbafcfd2da083b4852b581a327e624d9c
deni997 <deni.dujmovic97@gmail.com>
2022-04-04 23:04:17 +0200

    Add resource file reader class

---
+++ src/main/java/com/nsofttask/util/FileReader.java
@@ -0,0 +1,37 @@
+package com.nsofttask.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class FileReader {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileReader.class.getName());
+
+    public static String getResourceFileAsString(String fileName) {
+
+        String content = null;
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:data/" + fileName);
+        } catch (Exception e) {
+            LOGGER.error("File " + fileName + " not found");
+            e.printStackTrace();
+        }
+
+
+        try {
+            content = new String(Files.readAllBytes(file.toPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+
+
+}
