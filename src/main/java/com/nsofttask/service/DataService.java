b329f442ee9937d05fb5f1205197d61568fb233f
deni997 <deni.dujmovic97@gmail.com>
2022-04-05 19:15:49 +0200

    Data initialization

--- src/main/java/com/nsofttask/service/DataService.java
+++ src/main/java/com/nsofttask/service/DataService.java
@@ -2,26 +2,49 @@ package com.nsofttask.service;
 
 
 import com.nsofttask.util.FileReader;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataService {
 
+    private String markets;
+    private String events;
 
-    public void init() {
-        String markets = this.loadFile("markets.json");
-        String events = this.loadFile("events.json");
-        String market = this.loadFile("market.json");
-        String event = this.loadFile("event.json");
-        if(markets != null) System.out.println(markets);
-        if(events != null) System.out.println(events);
-        if(market != null) System.out.println(market);
-        if(event != null) System.out.println(event);
+    public void init() throws ParseException {
+        System.out.println("=============== Initializing data ===============");
+        this.markets = loadMarkets();
+        System.out.println(FileReader.convertStringToJson(this.markets));
+        this.events = loadEvents();
+        System.out.println(FileReader.convertStringToJson(this.events));
+        System.out.println("=============== Data initialized ===============");
+
     }
 
-    public String loadFile(String fileName) {
-        String file = FileReader.getResourceFileAsString(fileName);
-        return file;
+    public String getMarkets() {
+        return markets;
+    }
+
+    public String getEvents() {
+        return events;
+    }
+
+    public void setMarkets(String markets) {
+        this.markets = markets;
+    }
+
+    public void setEvents(String events) {
+        this.events = events;
+    }
+
+    private String loadMarkets() {
+        String markets = FileReader.getResourceFileAsString("markets.json");
+        return markets;
+    }
+
+    private String loadEvents() {
+        String events = FileReader.getResourceFileAsString("events.json");
+        return events;
     }
 
 }
