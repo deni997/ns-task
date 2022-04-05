6a6dc122feaf3f26c001552fb338c8fd3303f53a
deni997 <deni.dujmovic97@gmail.com>
2022-04-05 19:15:09 +0200

    Add GET which triggers message publishing by Producer and message consumin by Consumer

--- src/main/java/com/nsofttask/controller/DataController.java
+++ src/main/java/com/nsofttask/controller/DataController.java
@@ -1,35 +1,38 @@
 package com.nsofttask.controller;
 
 import com.nsofttask.service.DataService;
+import com.nsofttask.service.Producer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class DataController {
+
+    @Autowired
+    Producer producer;
 
     private final DataService dataService;
 
-    public DataController(DataService dataService) {
-        this.dataService = dataService;
+    @GetMapping("/get")
+    public void sendMessage() {
+        producer.publishToTopic();
     }
 
     @GetMapping("/events")
     public String getEvents() {
-        return dataService.loadFile("events.json");
+        System.out.println(dataService.getEvents());
+        return dataService.getEvents();
     }
 
     @GetMapping("/markets")
     public String getMarkets() {
-        return dataService.loadFile("markets.json");
+        System.out.println(dataService.getMarkets());
+        return dataService.getMarkets();
     }
 
-    @GetMapping("/market")
-    public String getMarket() {
-        return dataService.loadFile("market.json");
-    }
-
-    @GetMapping("/event")
-    public String getEvent() {
-        return dataService.loadFile("event.json");
-    }
 }
