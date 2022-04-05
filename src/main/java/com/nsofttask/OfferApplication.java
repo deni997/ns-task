1c909d55800def87235f0d62a9a5159b26ecf5e6
deni997 <deni.dujmovic97@gmail.com>
2022-04-05 23:40:08 +0200

    Running data initialization

--- src/main/java/com/nsofttask/OfferApplication.java
+++ src/main/java/com/nsofttask/OfferApplication.java
@@ -1,10 +1,18 @@
 package com.nsofttask;
 
+import com.google.gson.Gson;
+import com.nsofttask.enumerations.Status;
+import com.nsofttask.model.Market;
+import com.nsofttask.model.MarketOutcome;
 import com.nsofttask.service.DataService;
+import com.nsofttask.util.FileReader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class OfferApplication {
