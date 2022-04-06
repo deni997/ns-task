9453cc228ccbedc7c67d457c9cfb7c463d776bfc
deni997 <deni.dujmovic97@gmail.com>
2022-04-06 20:42:47 +0200

    Consuming market topic test

--- src/main/java/com/nsofttask/service/Consumer.java
+++ src/main/java/com/nsofttask/service/Consumer.java
@@ -2,6 +2,7 @@ package com.nsofttask.service;
 
 import com.google.gson.Gson;
 import com.nsofttask.model.Event;
+import com.nsofttask.model.Market;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
     @KafkaListener(topics = {"market", "event"}, groupId = "mygroup")
-    public void consumeFromTopic(String message) {
-        System.out.println("Consumed message:\n" + message);
+    public void consumeFromTopic(Market market) {
+        System.out.println("Consumed message:\n" + market);
     }
 }
