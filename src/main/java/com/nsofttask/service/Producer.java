75141521c07e77db3f5d7b36dbb2060860c9c80c
deni997 <deni.dujmovic97@gmail.com>
2022-04-06 20:44:13 +0200

    Add publish market to topic method

--- src/main/java/com/nsofttask/service/Producer.java
+++ src/main/java/com/nsofttask/service/Producer.java
@@ -1,6 +1,9 @@
 package com.nsofttask.service;
 
-import com.nsofttask.util.FileReader;
+import com.nsofttask.model.Event;
+import com.nsofttask.model.Market;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -8,16 +11,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class Producer {
 
-    private static final String marketTopic = "market";
-    private static final String marketData = FileReader.getResourceFileAsString("market.json");
-    private static final String eventTopic = "event";
-    private static final String eventData = FileReader.getResourceFileAsString("event.json");
+    private final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, Event> eventKafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, Market> marketKafkaTemplate;
 
-    public void publishToTopic(String topicName, String data) {
+    public void publishEventToTopic(String topicName, Event eventData) {
+        LOGGER.info("Sending Event Json Serializer : {}", eventData);
         System.out.println("Publishing to topic:\n" + topicName);
-        this.kafkaTemplate.send(topicName, data);
+        eventKafkaTemplate.send(topicName, eventData);
+    }
+
+    public void publishMarketToTopic(String topicName, Market marketData) {
+        LOGGER.info("Sending Event Json Serializer : {}", marketData);
+        System.out.println("Publishing to topic:\n" + topicName);
+        marketKafkaTemplate.send(topicName, marketData);
     }
 }
