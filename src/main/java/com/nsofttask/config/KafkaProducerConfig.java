bf62170b9de8178647978930ca820c8bcb43bd06
deni997 <deni.dujmovic97@gmail.com>
2022-04-06 20:44:54 +0200

    Add producer configuration

---
+++ src/main/java/com/nsofttask/config/KafkaProducerConfig.java
@@ -0,0 +1,54 @@
+package com.nsofttask.config;
+
+import com.nsofttask.model.Event;
+import com.nsofttask.model.Market;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    private String bootstrapServers = "localhost:9092";
+
+    @Bean
+    public ProducerFactory<String, Event> eventProducerFactory() {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        return new DefaultKafkaProducerFactory<>(configProperties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Event> eventKafkaTemplate() {
+        return new KafkaTemplate<>(eventProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, Market> marketProducerFactory() {
+        Map<String, Object> configProperties = new HashMap<>();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        return new DefaultKafkaProducerFactory<>(configProperties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Market> marketKafkaTemplate() {
+        return new KafkaTemplate<>(marketProducerFactory());
+    }
+
+}
