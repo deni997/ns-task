eed06c67ce312a6f24743412b6a39ca020b4e7e0
deni997 <deni.dujmovic97@gmail.com>
2022-04-07 17:36:41 +0200

    Edit consumer

--- src/main/java/com/nsofttask/service/Consumer.java
+++ src/main/java/com/nsofttask/service/Consumer.java
@@ -3,14 +3,25 @@ package com.nsofttask.service;
 import com.google.gson.Gson;
 import com.nsofttask.model.Event;
 import com.nsofttask.model.Market;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
-    @KafkaListener(topics = {"market", "event"}, groupId = "mygroup")
-    public void consumeFromTopic(Market market) {
+    @Autowired
+    DataService dataService;
+
+    @KafkaListener(topics = "event", groupId = "mygroup")
+    public void consumeEventTopic(Event event) {
+        dataService.updateEvents(event);
+        System.out.println("Event updated.");
+    }
+
+    @KafkaListener(topics = {"market"}, groupId = "mygroup")
+    public void consumeMarketTopic(Market market) {
         System.out.println("Consumed message:\n" + market);
     }
+
 }
