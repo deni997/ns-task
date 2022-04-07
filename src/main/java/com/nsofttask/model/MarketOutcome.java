f8ade77392b00c1a0029f79cb8749f750d117e3f
deni997 <deni.dujmovic97@gmail.com>
2022-04-07 20:39:19 +0200

    Add validation annotation

--- src/main/java/com/nsofttask/model/MarketOutcome.java
+++ src/main/java/com/nsofttask/model/MarketOutcome.java
@@ -2,13 +2,17 @@ package com.nsofttask.model;
 
 import com.google.gson.annotations.SerializedName;
 import com.nsofttask.enumerations.Status;
+import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 @Data
 public class MarketOutcome {
 
+    @NotNull
     private String id;
+    @NotNull
     private String name;
+    @NotNull
     private Status status;
 
 }
