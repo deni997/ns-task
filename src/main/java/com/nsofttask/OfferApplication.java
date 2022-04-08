8c4e38c1a199c35c590fd6e489f4bd5573fe51d5
deni997 <deni.dujmovic97@gmail.com>
2022-04-08 19:17:42 +0200

    Add EnableScheduling annotation

--- src/main/java/com/nsofttask/OfferApplication.java
+++ src/main/java/com/nsofttask/OfferApplication.java
@@ -10,11 +10,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 public class OfferApplication {
 
 	public static void main(String[] args) {
