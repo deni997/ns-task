887d1f2035ce4ae19a4e8974f181ed310c33295d
deni997 <deni.dujmovic97@gmail.com>
2022-04-08 14:46:35 +0200

    Add filtering by date

--- src/main/java/com/nsofttask/controller/DataController.java
+++ src/main/java/com/nsofttask/controller/DataController.java
@@ -5,6 +5,7 @@ import com.nsofttask.model.Market;
 import com.nsofttask.service.DataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,9 @@ public class DataController {
 
     private final DataService dataService;
 
-    @GetMapping("/events")
-    public List<Event> getEvents() {
-        return dataService.getEvents();
+    @GetMapping("/events/selectedDate={date}")
+    public List<Event> getEvents(@PathVariable("date") String date) {
+        return dataService.getEvents(date);
     }
 
     @GetMapping("/markets")
