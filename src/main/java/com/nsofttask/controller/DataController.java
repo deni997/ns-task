20b321462edfdce5ba30bcd4eb246af60b2206e2
deni997 <deni.dujmovic97@gmail.com>
2022-04-08 18:19:57 +0200

    Edit get events controller

--- src/main/java/com/nsofttask/controller/DataController.java
+++ src/main/java/com/nsofttask/controller/DataController.java
@@ -4,10 +4,7 @@ import com.nsofttask.model.Event;
 import com.nsofttask.model.Market;
 import com.nsofttask.service.DataService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,8 @@ public class DataController {
 
     private final DataService dataService;
 
-    @GetMapping("/events/selectedDate={date}")
-    public List<Event> getEvents(@PathVariable("date") String date) {
+    @GetMapping("/events")
+    public List<Event> getEvents(@RequestParam(required = false) String date) {
         return dataService.getEvents(date);
     }
 
